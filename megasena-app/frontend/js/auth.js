11258d244516192cc011c967f7f57b709f8f8e8d
JULIANO <pisjuliano@gmail.com>
2025-12-18 20:29:29 -0300

    correção api 4

--- megasena-app/frontend/js/auth.js
+++ megasena-app/frontend/js/auth.js
@@ -1,5 +1,5 @@
 async function register() {
-    await fetch(API + '/auth/register', {
+    await fetch(API + 'auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -14,7 +14,7 @@ async function register() {
 }
 
 async function login() {
-    const res = await fetch(`${API}/auth/login`, {
+    const res = await fetch(`${API}auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
