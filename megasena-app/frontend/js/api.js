9bddadb0ce302edc195b31a59e2c7cd1efafe011
JULIANO <pisjuliano@gmail.com>
2025-12-18 20:20:16 -0300

    Correção do authheader

--- megasena-app/frontend/js/api.js
+++ megasena-app/frontend/js/api.js
@@ -2,7 +2,12 @@ const API = 'https://bolaomega-production.up.railway.app';
 
 function authHeader() {
     const token = localStorage.getItem('token');
+
+    if (!token) {
+        return {};
+    }
+
     return {
-        Authorization: `Bearer ${token}`
+        Authorization: 'Bearer ' + token
     };
 }
