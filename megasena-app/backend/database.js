e0928a94b93512a098a57b5ed987a71c8858731d
JULIANO <pisjuliano@gmail.com>
2025-12-21 11:39:42 -0300

    correções gerais

--- megasena-app/backend/database.js
+++ megasena-app/backend/database.js
@@ -2,7 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = {
